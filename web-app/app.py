714e7cd81b45f636e2e84bd86db23c479edee573
Hugo Rialan <hugo.rialan@eleve.isep.fr>
2022-03-11 12:15:50 +0100

    zert

--- web-app/app.py
+++ web-app/app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+import model
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        prediction, probability = model.get_prediction(request.form['image_url'])
+        
+        return render_template('index.html', 
+                                prediction=prediction,
+                                probability=probability)
+
     return render_template('index.html')
+    
 
 if __name__ == "__main__":
     app.run(host='localhost', debug=True)
